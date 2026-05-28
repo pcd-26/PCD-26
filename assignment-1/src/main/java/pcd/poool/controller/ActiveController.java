f286aeb442c264289aa8028ac224158021d89fef
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2026-05-28 18:14:49 +0200

    feat(assignment-1): add InterruptedException handler

--- assignment-1/src/main/java/pcd/poool/controller/ActiveController.java
+++ assignment-1/src/main/java/pcd/poool/controller/ActiveController.java
@@ -13,31 +13,42 @@ public class ActiveController<T> extends Thread {
 
     private final BoundedBuffer<Cmd<T>> cmdBuffer;
     private final T target;
+    private volatile boolean running;
 
     public ActiveController(T target, int queueSize) {
         this.cmdBuffer = new BoundedBufferImpl<>(queueSize);
         this.target = target;
+        this.running = true;
     }
 
     @Override
     public void run() {
         log("started");
-        while (true) {
+        while (running) {
             try {
                 Cmd<T> cmd = cmdBuffer.get();
                 cmd.execute(target);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+                running = false;
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
+        log("stopped");
     }
 
     public void notifyNewCmd(Cmd<T> cmd) {
         try {
             cmdBuffer.put(cmd);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
         }
+    }
+
+    public void shutdown() {
+        running = false;
+        interrupt();
     }
 
     private void log(String msg) {
