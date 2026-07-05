a40a7493325a2721e567403e91742e59f372b40a
Francesco Valentini <fv26012001@gmail.com>
2026-07-05 19:33:58 +0200

    Added even more doc

--- assignment-1/src/test/java/pcd/poool/model/physics/common/BoardPhysicsStepperTest.java
+++ assignment-1/src/test/java/pcd/poool/model/physics/common/BoardPhysicsStepperTest.java
@@ -11,6 +11,10 @@ import pcd.poool.model.common.math.V2d;
 
 class BoardPhysicsStepperTest {
 
+    /**
+     * Verifies that Board's updateState() method correctly delegates the step simulation
+     * execution to the injected PhysicsStepper strategy implementation.
+     */
     @Test
     void boardDelegatesUpdatesToInjectedPhysicsStepper() {
         var elapsed = new AtomicLong();
@@ -22,6 +26,10 @@ class BoardPhysicsStepperTest {
         assertEquals(42, elapsed.get());
     }
 
+    /**
+     * Verifies that the Board constructor throws an IllegalArgumentException if a null
+     * PhysicsStepper strategy is supplied.
+     */
     @Test
     void boardRejectsNullPhysicsStepper() {
         assertThrows(IllegalArgumentException.class, () -> new Board(null));
