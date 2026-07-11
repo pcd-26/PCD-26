a66542eb44da5e6cdad159d436e74a5b63cf7cce
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2026-07-11 22:47:30 +0200

    feat(assignment-3): add hotc command-line interface

--- assignment-3/hotc/src/main_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestApplicationName(t *testing.T) {
-	if applicationName != "Heads-or-Tails Championship" {
-		t.Fatalf("unexpected application name: %q", applicationName)
-	}
-}
