381613332a8e70b8584ac5ab9280a5195a810f75
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2026-07-11 22:39:36 +0200

    feat(assignment-3): add configurable control unit timers

--- assignment-3/shas/src/test/java/pcd/shas/ControlUnitActorTest.java
+++ assignment-3/shas/src/test/java/pcd/shas/ControlUnitActorTest.java
@@ -1,9 +1,10 @@
 package pcd.shas;
 
 import org.apache.pekko.actor.testkit.typed.javadsl.ActorTestKit;
+import org.apache.pekko.actor.testkit.typed.javadsl.ManualTime;
 import org.apache.pekko.actor.testkit.typed.javadsl.TestProbe;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pcd.shas.common.AlarmState;
 import pcd.shas.common.SensorInfo;
@@ -17,96 +18,102 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ControlUnitActorTest {
 
-    private static ActorTestKit testKit;
+    private ActorTestKit testKit;
+    private ManualTime manualTime;
 
-    @BeforeAll
-    static void setUp() {
-        testKit = ActorTestKit.create();
+    @BeforeEach
+    void setUp() {
+        testKit = ActorTestKit.create("control-unit-tests", ManualTime.config());
+        manualTime = ManualTime.get(testKit.system());
     }
 
-    @AfterAll
-    static void tearDown() {
+    @AfterEach
+    void tearDown() {
         testKit.shutdownTestKit();
     }
 
     @Test
     void initialStateIsDisarmed() {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(50), Duration.ofMillis(50)));
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(5), Duration.ofSeconds(5)));
 
         assertState(controlUnit, AlarmState.DISARMED);
     }
 
     @Test
-    void correctPinMovesSystemToExitDelay() {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(50), Duration.ofMillis(50)));
+    void correctPinMovesSystemToExitDelayAndExitTimeoutArmsSystem() {
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(1), Duration.ofSeconds(1)));
 
         controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
-
-        assertState(controlUnit, AlarmState.EXIT_DELAY);
-    }
-
-    @Test
-    void incorrectPinHasNoEffectWhileDisarmed() {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(50), Duration.ofMillis(50)));
-
-        controlUnit.tell(new ControlUnitActor.PinSubmitted("0000"));
-
-        assertState(controlUnit, AlarmState.DISARMED);
-    }
-
-    @Test
-    void exitDelayIgnoresPinSubmissionsUntilExpiration() throws Exception {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(40), Duration.ofMillis(40)));
-
-        controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
-        controlUnit.tell(new ControlUnitActor.PinSubmitted("0000"));
-
         assertState(controlUnit, AlarmState.EXIT_DELAY);
 
-        controlUnit.tell(new ControlUnitActor.ExitDelayExpired());
-
+        manualTime.timePasses(Duration.ofSeconds(1));
         assertState(controlUnit, AlarmState.ARMED);
     }
 
     @Test
-    void armedSensorMovesSystemToEntryDelay() {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(40), Duration.ofMillis(40)));
+    void incorrectPinHasNoEffectWhileDisarmed() {
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(1), Duration.ofSeconds(1)));
 
-        controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
-        controlUnit.tell(new ControlUnitActor.ExitDelayExpired());
+        controlUnit.tell(new ControlUnitActor.PinSubmitted("0000"));
 
-        controlUnit.tell(new ControlUnitActor.SensorActivated(new SensorInfo("front_door", SensorType.DOOR_WINDOW, "Perimeter")));
-
-        assertState(controlUnit, AlarmState.ENTRY_DELAY);
+        assertState(controlUnit, AlarmState.DISARMED);
     }
 
     @Test
-    void entryDelayExpiresIntoAlarmAndCorrectPinDisarms() {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(40), Duration.ofMillis(40)));
+    void leavingExitDelayCancelsObsoleteTimeout() {
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(1), Duration.ofSeconds(1)));
 
         controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
-        controlUnit.tell(new ControlUnitActor.ExitDelayExpired());
-        controlUnit.tell(new ControlUnitActor.SensorActivated(new SensorInfo("front_door", SensorType.DOOR_WINDOW, "Perimeter")));
+        assertState(controlUnit, AlarmState.EXIT_DELAY);
 
+        controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
+        assertState(controlUnit, AlarmState.DISARMED);
+
+        manualTime.timePasses(Duration.ofSeconds(1));
+        assertState(controlUnit, AlarmState.DISARMED);
+    }
+
+    @Test
+    void armedSensorMovesSystemToEntryDelayAndEntryTimeoutAlarmsSystem() {
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(1), Duration.ofSeconds(1)));
+
+        controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
+        manualTime.timePasses(Duration.ofSeconds(1));
+        assertState(controlUnit, AlarmState.ARMED);
+
+        controlUnit.tell(new ControlUnitActor.SensorActivated(new SensorInfo("front_door", SensorType.DOOR_WINDOW, "Perimeter")));
         assertState(controlUnit, AlarmState.ENTRY_DELAY);
 
-        controlUnit.tell(new ControlUnitActor.EntryDelayExpired());
-
+        manualTime.timePasses(Duration.ofSeconds(1));
         assertState(controlUnit, AlarmState.ALARM);
+    }
+
+    @Test
+    void entryDelayPinDisarmsBeforeTimeout() {
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(1), Duration.ofSeconds(1)));
 
         controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
+        manualTime.timePasses(Duration.ofSeconds(1));
+        controlUnit.tell(new ControlUnitActor.SensorActivated(new SensorInfo("front_door", SensorType.DOOR_WINDOW, "Perimeter")));
+        assertState(controlUnit, AlarmState.ENTRY_DELAY);
 
+        controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
+        assertState(controlUnit, AlarmState.DISARMED);
+
+        manualTime.timePasses(Duration.ofSeconds(1));
         assertState(controlUnit, AlarmState.DISARMED);
     }
 
     @Test
     void incorrectPinHasNoEffectInAlarm() {
-        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofMillis(40), Duration.ofMillis(40)));
+        var controlUnit = testKit.spawn(ControlUnitActor.create("1234", Duration.ofSeconds(1), Duration.ofSeconds(1)));
 
         controlUnit.tell(new ControlUnitActor.PinSubmitted("1234"));
-        controlUnit.tell(new ControlUnitActor.ExitDelayExpired());
+        manualTime.timePasses(Duration.ofSeconds(1));
         controlUnit.tell(new ControlUnitActor.SensorActivated(new SensorInfo("front_door", SensorType.DOOR_WINDOW, "Perimeter")));
-        controlUnit.tell(new ControlUnitActor.EntryDelayExpired());
+        manualTime.timePasses(Duration.ofSeconds(1));
+
+        assertState(controlUnit, AlarmState.ALARM);
 
         controlUnit.tell(new ControlUnitActor.PinSubmitted("0000"));
 
@@ -118,7 +125,7 @@ class ControlUnitActorTest {
         assertThrows(IllegalArgumentException.class, () -> ControlUnitActor.create(" "));
     }
 
-    private static void assertState(org.apache.pekko.actor.typed.ActorRef<ControlUnitActor.Command> controlUnit, AlarmState expected) {
+    private void assertState(org.apache.pekko.actor.typed.ActorRef<ControlUnitActor.Command> controlUnit, AlarmState expected) {
         TestProbe<ControlUnitActor.StateSnapshot> stateProbe = testKit.createTestProbe(ControlUnitActor.StateSnapshot.class);
         controlUnit.tell(new ControlUnitActor.QueryState(stateProbe.getRef()));
         assertEquals(expected, stateProbe.receiveMessage().state());
