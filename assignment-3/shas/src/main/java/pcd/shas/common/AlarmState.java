6efcd3ac26f4cd5b1becdc4a3aaa4b292bf8f5a8
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2026-07-11 22:30:26 +0200

    feat(assignment-3): define alarm domain and control protocol

---
+++ assignment-3/shas/src/main/java/pcd/shas/common/AlarmState.java
@@ -0,0 +1,12 @@
+package pcd.shas.common;
+
+/**
+ * High-level states of the smart home alarm system.
+ */
+public enum AlarmState {
+    DISARMED,
+    EXIT_DELAY,
+    ARMED,
+    ENTRY_DELAY,
+    ALARM
+}
