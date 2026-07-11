1947282ad302c5aed7f934d8358a06f4d9253c06
Francesco Valentini <fv26012001@gmail.com>
2026-07-11 20:45:43 +0200

    Add test config to Ass2

---
+++ assignment-2/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Find the directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Change directory to the script's folder
+cd "$SCRIPT_DIR"
+
+# Run tests
+mvn test
